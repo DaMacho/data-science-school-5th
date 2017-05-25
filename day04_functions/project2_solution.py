51621df545d509a88ffd3c01ca69e4f1669a9fdd
aaron byun <macmath22@gmail.com>
2017-05-25 10:24:53 +0900

    update lecture notes

--- day04_functions/project2_solution.py
+++ day04_functions/project2_solution.py
@@ -2,21 +2,23 @@
 
 import random
 
-# puzzle을 아래와 같이 보기좋은 모양으로 출력함
-# 1 2 3
-# 4 5 6
-# 7 8
 def show_puzzle(puzzle):
-    pass
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[0])):
+            print '{:2}'.format(puzzle[i][j]),
+        print
 
-# 퍼즐을 생성한다.
-# 퍼즐은 동적으로 생성되며, 2차원 리스트 형태를 갖는다
-# 즉 [[1, 2, 3], [4, 5, 6], [7, 8, '']]
-# 빈공간은 ''로 표현한다
 def initiate_puzzle(size):
-    pass
+    puzzle = []
+    for i in range(size):
+        row = []
+        for j in range(size):
+            row.append(i*size + j+1)
+        puzzle.append(row)
 
-# 퍼즐을 랜덤하게 섞음
+    puzzle[-1][-1] = ''
+    return puzzle
+
 def shuffle_puzzle(puzzle):
     dxs = [1, 0, -1,  0]
     dys = [0, 1,  0, -1]
@@ -35,34 +37,44 @@ def shuffle_puzzle(puzzle):
             puzzle[ni][nj], puzzle[i][j] = puzzle[i][j], puzzle[ni][nj]
         cnt += 1
 
-# 퍼즐이 종료되었는지, 즉 차례대로 정렬되어 완료가 되었는지 검사
 def is_done(puzzle, complete):
-    pass
+    return puzzle == complete
 
-# 퍼즐 보드에서 숫자 n의 인덱스 검색
-# 2차원이기 때문에 i, j형태의 인덱스 값을 반환
 def get_index(puzzle, n):
-    pass
+    for i in range(len(puzzle)):
+        try:
+            index = puzzle[i].index(n)
+            return i, index
+        except:
+            continue
+    return -1, -1
 
-# 퍼즐에서 숫자 n을 이동,
-# 이동할 수 없는 경우에는 이동할 수 없다고 표시
-# 이동이 가능한 경우는 양옆위아래에 ''가 위치해 있을 경우이다
 def move_by_number(puzzle, n):
-    pass
+    i, j = get_index(puzzle, n)
+    move_by_index(puzzle, i, j)
 
-# 숫자를 이동시키기 위해서는 결국 인덱스를 알아야 함
-# 즉 move_by_number 내부에서 호출되는 함순
 def move_by_index(puzzle, i, j):
     # movable condition
     # 좌우위아래 한방향중 하나가 '' 값이라면 이동 가능
-    pass
+    dxs = [1, 0, -1,  0]
+    dys = [0, 1,  0, -1]
 
+    for dx, dy in zip(dxs, dys):
+        new_i = i + dx
+        new_j = j + dy
+
+        if not (0 <= new_i < len(puzzle) and 0 <= new_j < len(puzzle[0])):
+            continue
+
+        if puzzle[new_i][new_j] == '':
+            puzzle[i][j], puzzle[new_i][new_j] = puzzle[new_i][new_j], puzzle[i][j]
+            break
+    else:
+        print 'sorry, you cannot move that number'
 
 # 퍼즐 생성
 size = int(raw_input(' -> please insert puzzle size : '))
 puzzle = initiate_puzzle(size)
-
-# 연습문제) -> 아래와 같이 복사하는 이유는?
 complete = [row[:] for row in puzzle]
 
 # 퍼즐 섞기
@@ -79,7 +91,7 @@ while not is_done(puzzle, complete):
 
     # 화면 clear
     import os
-    cls = lambda: os.system('clear') # windows : cls
+    cls = lambda: os.system('clear') # windows cls
     cls()
 
     # 움직인 이후 퍼즐 상태 보기
